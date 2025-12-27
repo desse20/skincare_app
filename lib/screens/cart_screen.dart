412c1003c2f67379e6d764c5228a7c0962666d2f
ISSOUMA Nafissath <issoumanafissath@gmail.com>
2025-12-27 15:35:56 +0100

    fix: mise à jour de la navigation

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_bottom_nav.dart';
+import '../routes/app_routes.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -11,7 +12,7 @@ class CartScreen extends StatelessWidget {
         title: const Text('Panier'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
+           onPressed: () => Navigator.pushNamed(context, AppRoutes.home),
         ),
       ),
       body: const Center(child: Text('Cart Screen - À implémenter')),
