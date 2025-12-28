66fa0966c8c811c6684482defc05e2d67c190607
Jean-Marie <oktcjean@gmail.com>
2025-12-28 23:00:46 +0100

    fonctionnalité des detail produit

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,14 +2,16 @@ class Product {
   final String id;
   final String name;
   final String price;
-  final String imagePath;
+  final List<String> images;
+  final String capacity;
   final String description;
 
   const Product({
     required this.id,
     required this.name,
     required this.price,
-    required this.imagePath,
+    required this.images,
+    required this.capacity,
     this.description = '',
   });
 }
