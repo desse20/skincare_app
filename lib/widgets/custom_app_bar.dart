47bceb00e118f18d92016d3f58acd21a16127a7d
HOUNDENOU <conceptiahoundenou@gmail.com>
2025-12-31 18:24:36 +0100

    mise à jour du fichier

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,6 +1,7 @@
 // Pour l'AppBar avec le logo et le panier.
 import 'package:flutter/material.dart';
-import '../routes/app_routes.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart_provider.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -14,6 +15,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    int count = context.watch<CartProvider>().totalCount;
     return AppBar(
       title: Text(
         title, 
@@ -25,16 +27,44 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       iconTheme: const IconThemeData(color: Colors.black),
       actions: [
         if (showCart)
-          IconButton(
-            icon: const Icon(Icons.shopping_cart_outlined),
-            onPressed: () {
-
-              Navigator.pushNamed(context, AppRoutes.cart);
-            },
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.shopping_cart_outlined),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              if (count > 0) // On n'affiche le badge que si le panier n'est pas vide
+                Positioned(
+                  right: 8,
+                  top: 8,
+                  child: Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    constraints: const BoxConstraints(
+                      minWidth: 16,
+                      minHeight: 16,
+                    ),
+                    child: Text(
+                      '$count',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+            ],
           ),
       ],
     );
-  }
+
+  }  
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
