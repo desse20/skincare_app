6e347148b0fed462afdd7d3e8fb6f608cbf528ea
HOUNDENOU <conceptiahoundenou@gmail.com>
2025-12-31 18:16:44 +0100

    feat: ajout du catalogue de produits et des données statiques

---
+++ lib/data/product_data.dart
@@ -0,0 +1,20 @@
+import '../models/product.dart';
+
+const List<Product> skincareProducts = [
+  Product(
+    id: '1',
+    name: 'Re:dence',
+    price: '160',
+    images: ['assets/images/product1.png'],
+    capacity: '50ml',
+    description: 'Sérum anti-âge Re:dence, raffermit et illumine la peau.',
+  ),
+  Product(
+    id: '2',
+    name: 'Greenling',
+    price: '150',
+    images: ['assets/images/product2.jpg'],
+    capacity: '100ml',
+    description: 'Crème hydratante Greenling, fraîcheur et éclat naturel.',
+  ),
+];
