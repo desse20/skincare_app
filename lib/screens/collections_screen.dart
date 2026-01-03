312b60ccc89014ccf28406f04cbcf88ab57c4d1b
ISSOUMA Nafissath <issoumanafissath@gmail.com>
2026-01-03 12:17:31 +0100

    mise à jour

--- lib/screens/collections_screen.dart
+++ lib/screens/collections_screen.dart
@@ -21,16 +21,6 @@ class CollectionsScreen extends StatelessWidget {
 
         return Scaffold(
           backgroundColor: Colors.grey[50],
-          // appBar: AppBar(
-          //   title: const Text('My Favorites'),
-          //   backgroundColor: Colors.white,
-          //   foregroundColor: Colors.black,
-          //   elevation: 0,
-          //   leading: IconButton(
-          //     icon: const Icon(Icons.arrow_back),
-          //     onPressed: () => Navigator.pop(context),
-          //   ),
-          // ),
           appBar: const CustomAppBar(
             title: 'My Favorites',
             showBackButton: true, 
