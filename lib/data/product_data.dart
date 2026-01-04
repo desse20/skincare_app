8bec91c6251e8d383e86d14c4863246c99641a8a
HOUNDENOU <conceptiahoundenou@gmail.com>
2026-01-04 21:14:27 +0100

    data: ajout des nouveaux produits (ID 3 à 6) au catalogue

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -19,4 +19,44 @@ const List<Product> skincareProducts = [
     category: 'Women',
     description: 'Crème hydratante Greenling, fraîcheur et éclat naturel.',
   ),
-];
+
+Product(
+      id: '3',
+      name: 'Men Face Wash',
+      price: '45',
+      images: ['assets/images/product_man.png'],
+      capacity: '100ml',
+      category: 'Man',
+      description: 'Deep cleansing face wash for men.',
+    ),
+    
+    Product(
+      id: '4',
+      name: 'Baby Lotion',
+      price: '25',
+      images: ['assets/images/product_kids.png'],
+      capacity: '200ml',
+      category: 'Kids',
+      description: 'Gentle lotion for delicate baby skin.',
+    ),
+    Product(
+      id: '5',
+      name: 'Family Cream',
+      price: '60',
+      images: ['assets/images/product_parents.png'],
+      capacity: '450ml',
+      category: 'Parents',
+      description: 'Moisturizing cream for the whole family.',
+    ),
+    Product(
+      id: '6',
+      name: 'Lumière Serum',
+      price: '180',
+      images: ['assets/images/product_women.png'],
+      capacity: '30ml',
+      category: 'Women',
+      description: 'Radiance boosting serum.',
+    ),
+
+  ];
+
